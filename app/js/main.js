8be79629e671e3004acc7cea87e7c830deda5e43
Anastasia Pirogova <109249070+Nastia-Pirogova@users.noreply.github.com>
2023-09-24 19:16:30 +0300

    add

--- app/js/main.js
+++ app/js/main.js
@@ -9,3 +9,38 @@ titlePrograms.forEach((titleProgram, index) => {
         titlePrograms[index].classList.toggle("spec-programs-item-title-active");
     });
 });
+
+
+(() => {
+    const mobileMenu = document.querySelector('.js-menu-container');
+    const openMenuBtn = document.querySelector('.js-open-menu');
+    const closeMenuBtn = document.querySelector('.js-close-menu');
+    const menuItems = document.querySelectorAll('.mobile-menu__item');
+
+    const toggleMenu = () => {
+        const isMenuOpen =
+            openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+        openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+        mobileMenu.classList.toggle('is-open');
+
+        const scrollLockMethod = !isMenuOpen
+            ? 'disableBodyScroll'
+            : 'enableBodyScroll';
+        // bodyScrollLock[scrollLockMethod](document.body);
+    };
+
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            toggleMenu(); // Закрывает модальное окно при нажатии на элемент меню
+        });
+    });
+
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+        if (!e.matches) return;
+        mobileMenu.classList.remove('is-open');
+        openMenuBtn.setAttribute('aria-expanded', false);
+    });
+})();
